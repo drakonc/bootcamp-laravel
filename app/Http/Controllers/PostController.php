bd6821d0d5dea585e192c9f118f46dc2583a913d
Jose Castro <drakonc@gmail.com>
2022-09-30 08:06:33 -0500

    Post

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -4,18 +4,29 @@ namespace App\Http\Controllers;
 
 use App\Models\Post;
 use Illuminate\Http\Request;
+use Inertia\Inertia;
 
 class PostController extends Controller
 {
 
     public function index()
     {
-        //
+        return Inertia::render('Post/Index',[
+
+        ]);
     }
 
     public function store(Request $request)
     {
-        //
+        //Validamos los Datos
+        $validated = $request->validate([
+            'title' => 'required|string|max:100',
+            'body' => 'required|string|max:255'
+        ]);
+
+        $request->user()->posts()->create($validated);
+
+        return redirect(route('posts.index'));
     }
 
     public function update(Request $request, Post $post)

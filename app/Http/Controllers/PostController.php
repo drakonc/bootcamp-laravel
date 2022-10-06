a13e4360d627e020712a11a128f6868659a5b21c
Jose Castro <drakonc@gmail.com>
2022-10-06 14:26:19 -0500

    Subiendo Ultimos Cambios

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -12,7 +12,7 @@ class PostController extends Controller
     public function index()
     {
         return Inertia::render('Post/Index',[
-
+            'post' => Post::with('user:id,name')->latest()->get()
         ]);
     }
 
--- app/Models/Post.php
+++ app/Models/Post.php
@@ -11,7 +11,7 @@ class Post extends Model
 
     protected $fillable = ['title','body'];
 
-    public function user(){
-        return $this->bil ;
+    public function user() {
+        return $this->belongsTo(User::class);
     }
 }
